60e21f5695f6f2f14ad4918f99dc6b113a70d40d
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-12-10 11:42:23 +0500

    task is done

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1-masal============================================
+// function arrayMadness(a, b) {
+//   let first = a
+//     .map((value) => value ** 3)
+//     .reduce((acc, value) => acc + value, 0);
+//   let secont = b
+//     .map((value) => value ** 2)
+//     .reduce((acc, value) => acc + value, 0);
+//   console.log(first);
+//   console.log(secont);
+
+//   return first > secont ? true : false;
+// }
+// console.log(arrayMadness([2, 3, 4], [1, 2, 3]));
+// 2-masala============================================
+function printArray(array) {
+  let newarr = "";
+  newarr += array;
+  return newarr;
+}
+console.log(printArray(["h", "o", "l", "a"]));
+// 3-masala================================================
+// let sum = 0;
+// let average;
+// function calctheaverage(numbers) {
+//   for (const number of numbers) {
+//     sum += number;
+//   }
+//   average = sum / numbers.length;
+//   return average;
+// }
+// console.log(calctheaverage([1, 3]));
+// 4 - masala;
+// let newarr = "";
+// function stringToArray(string) {
+//   newarr = string.split(" ");
+//   return newarr;
+// }
+// console.log(stringToArray("I love arrays they are my favorite"));
+// 5-masala==============================================================
+// function findminimum(params) {
+//   let minnumber = Math.min(...params);
+//   return minnumber;
+// }
+// console.log(findminimum([34, 15, 88, 2]));
